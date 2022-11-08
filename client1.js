c152cf8dc0cb88c313e5d4834f670d91b14577c2
minhlexuan1909 <minhlx1909@gmail.com>
2022-11-09 00:33:01 +0700

    [UPDATE] Demo client

---
+++ client1.js
@@ -0,0 +1,35 @@
+const { WebSocket } = require("ws");
+
+var connection = new WebSocket("ws://127.0.0.1:4444");
+
+const MqttMessageType = {
+  CONNECT: 1,
+};
+
+connection.onopen = function () {
+  console.log("Connected!");
+  // connection.send("Ping"); // Send the message 'Ping' to the server
+  // const connectInfo = {
+  //   clientId: 431,
+  //   topic: "B19DCCN431",
+  //   type: MqttMessageType.CONNECT,
+  //   payload: {},
+  // };
+  const subscribeInfo = {
+    clientId: 430,
+    topic: "B19DCCN431",
+    type: 3,
+    payload: {},
+  };
+  connection.send(JSON.stringify(subscribeInfo));
+};
+
+// Log errors
+connection.onerror = function (error) {
+  console.log("WebSocket Error " + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+  console.log("Server: " + e.data);
+};
